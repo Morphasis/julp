7621712ac097261edb982f4a4da3167cbdcf8ae9
Jack Sharville <jacksharville@gmail.com>
2016-09-12 21:21:47 +0100

    watch now watches js , css and html

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,8 +2,9 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
 gulp.task('watch', ['browserSync', 'sass'], function (){
-  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('../src/scss/**/*.scss', ['sass']);
   gulp.watch('../src/*.html', browserSync().reload);
   gulp.watch('../src/**/*.html', browserSync().reload);
+  gulp.watch('../src/js/**/*.js', browserSync().reload);
   // Other watchers
 });
