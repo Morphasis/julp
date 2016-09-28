c4641277159acf9ea4bf2554938603d9b985d0a8
Jack Sharville <jacksharville@gmail.com>
2016-09-28 20:14:13 +0100

    temp mess but it fixes all the reloads

--- gulp/tasks/browser.js
+++ gulp/tasks/browser.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
 gulp.task('browserSync', function() {
@@ -8,3 +9,24 @@ gulp.task('browserSync', function() {
     },
   })
 })
+
+gulp.task('watch', function (){
+  gulp.watch('../src/scss/**/*.scss', ['sass']);
+  gulp.watch('../src/*.html', browserSync.reload);
+  gulp.watch('../src/**/*.html', browserSync.reload);
+  gulp.watch('../src/javascript/**/*.js', browserSync.reload);
+  // Other watchers
+});
+
+// gulp.task('sass', function(){
+//   return gulp.src('../src/scss/**/*.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest('../src/css'))
+// });
+
+gulp.task('sass', function() {
+    return gulp.src("../src/scss/**/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("../src/css"))
+        .pipe(browserSync.stream());
+});
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,10 +1,2 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
-
-gulp.task('watch', ['browserSync', 'sass'], function (){
-  gulp.watch('../src/scss/**/*.scss', ['sass']);
-  gulp.watch('../src/*.html', browserSync().reload);
-  gulp.watch('../src/**/*.html', browserSync().reload);
-  gulp.watch('../src/js/**/*.js', browserSync().reload);
-  // Other watchers
-});
+// var gulp = require('gulp');
+// var browserSync = require('browser-sync');
