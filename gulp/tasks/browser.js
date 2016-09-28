2d00e1450fc6ffb7b836e627cfe1714c22f4dab5
Jack Sharville <jacksharville@gmail.com>
2016-09-28 21:06:17 +0100

    this clearly needs to be tidyed up but is a way of live injecting html

--- gulp/tasks/browser.js
+++ gulp/tasks/browser.js
@@ -1,19 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var htmlInjector = require("bs-html-injector");
 
 gulp.task('browserSync', function() {
+  browserSync.use(htmlInjector, {
+      // Files to watch that will trigger the injection
+      files: "../src/*.html"
+  });
   browserSync.init({
     server: {
       baseDir: '../src'
     },
-  })
+  });
 })
 
 gulp.task('watch', function (){
   gulp.watch('../src/scss/**/*.scss', ['sass']);
-  gulp.watch('../src/*.html', browserSync.reload);
-  gulp.watch('../src/**/*.html', browserSync.reload);
+  // gulp.watch('../src/*.html', htmlInjector);
+  // gulp.watch('../src/**/*.html', htmlInjector);
   gulp.watch('../src/javascript/**/*.js', browserSync.reload);
   // Other watchers
 });
